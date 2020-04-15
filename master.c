d470dc5796974a74f842438c008a29c7318a0cb8
dsalex1 <dsalex1@ymail.com>
2020-04-15 03:25:10 +0200

    added temp broadcast/processing

--- master.c
+++ master.c
@@ -7,6 +7,7 @@
 #include "net/nullnet/nullnet.h"
 
 #include <random.h>
+#include <string.h>
 #include <stdio.h>
 
 /*---------------------------------------------------------------------------*/
@@ -14,7 +15,53 @@ PROCESS(lab1_master, "Lab1 Master");
 AUTOSTART_PROCESSES(&lab1_master);
 
 /*---------------------------------------------------------------------------*/
-void toggleLedAndBroadcast(uint16_t led)
+void input_callback(const void *data, uint16_t len,
+                    const linkaddr_t *src, const linkaddr_t *dest)
+{
+  static int16_t receivedData[16];
+  static int16_t receivedFlags = 0;
+  static int16_t receivedCount = 0;
+
+  if (len == sizeof(int16_t))
+  {
+    int8_t srcId = src->u8[0];
+    if (srcId > 15)
+    {
+      printf("ERROR, cant process data from nodes with id>15: %d", srcId);
+      return;
+    }
+
+    if (((receivedFlags >> srcId) & 1) == 1) //already received from this node, guess we got all in this iteration
+    {
+      int16_t average = 0;
+      printf("Temperatures: ");
+
+      int i = 0;
+      for (i = 0; i < receivedCount; i++)
+      {
+        average += receivedData[i];
+        printf("%d", receivedData[i]);
+        if (i != receivedCount - 1)
+          printf(", ");
+      }
+      average /= receivedCount;
+      printf("\nAverage: %d\n", average);
+
+      receivedCount = 0;
+      receivedFlags = 0;
+    }
+
+    int16_t temp;
+    memcpy(&temp, data, sizeof(temp));
+
+    receivedData[receivedCount] = temp;
+    receivedCount++;
+    receivedFlags |= 1 << srcId;
+  }
+}
+
+/*---------------------------------------------------------------------------*/
+void toggleLedAndBroadcast(uint8_t led)
 {
   leds_toggle(led);
   nullnet_buf = (uint8_t *)&led;
@@ -30,6 +77,9 @@ PROCESS_THREAD(lab1_master, ev, data)
   static struct etimer et;
 
   etimer_set(&et, CLOCK_SECOND);
+
+  /* Initialize NullNet */
+  nullnet_set_input_callback(input_callback);
 
   while (1)
   {
--- slave.c
+++ slave.c
@@ -2,7 +2,9 @@
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
 #include "dev/leds.h"
+#include "node-id.h"
 
+#include <random.h>
 #include <string.h>
 
 /*---------------------------------------------------------------------------*/
@@ -13,24 +15,47 @@ AUTOSTART_PROCESSES(&lab1_slave);
 void input_callback(const void *data, uint16_t len,
                     const linkaddr_t *src, const linkaddr_t *dest)
 {
-    if (len == sizeof(unsigned))
+    if (len == sizeof(uint8_t))
     {
         unsigned int led;
         memcpy(&led, data, sizeof(led));
         leds_toggle(led);
     }
 }
+/*---------------------------------------------------------------------------*/
+void sendTemp(int16_t temp)
+{
+    nullnet_buf = (uint8_t *)&temp;
+    nullnet_len = sizeof(temp);
+    NETSTACK_NETWORK.output(NULL);
+}
+
+unsigned short rseed;
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(lab1_slave, ev, data)
 {
     PROCESS_BEGIN();
 
+    static struct etimer et;
+
+    etimer_set(&et, CLOCK_SECOND);
+
+    /*make random actually random...damn hardcoded seed*/
+    random_init(node_id);
     /* Initialize NullNet */
     nullnet_set_input_callback(input_callback);
 
     while (1)
+    {
         PROCESS_YIELD();
+
+        if (ev == PROCESS_EVENT_TIMER && data == &et)
+        {
+            sendTemp(random_rand() % 200 - 100);
+            etimer_set(&et, CLOCK_SECOND);
+        }
+    }
 
     PROCESS_END();
 }
